3e11fd104532b65ae77588395f8d9a8c61397985
connor-jason <connorejason@gmail.com>
2025-02-15 23:09:49 -0500

    some basic functions

--- Lake_Morts.py
+++ Lake_Morts.py
@@ -43,27 +43,54 @@ class Game:
 
 
 class Lasker_Morris(Game):
-#Empty for now 
     def __init__(self, h=3, v=3, k=3):
-        self.h = h
-        self.v = v
-        self.k = k
-        # a1, a4, a7, b2, b4, b6, c3, c4, c5, d1, d2, d3, d5, d6, d7, e3, e4, e5, f2, f4, f6, g1, g4, g7
-        moves = [(x, y) for x in range(1, h + 1)
-                 for y in range(1, v + 1)]
-        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
+        # Board positions
+        self.positions = ["a1", "a4", "a7", "b2", "b4", "b6",
+                          "c3", "c4", "c5", "d1", "d2", "d3",
+                          "d5", "d6", "d7", "e3", "e4", "e5",
+                          "f2", "f4", "f6", "g1", "g4", "g7"]
+        
+        # Initialize the board
+        board = {pos: None for pos in self.positions}
+
+        # All positions are initially available for placement
+        moves = self.positions.copy()
+
+        self.initial = GameState(to_move='X', utility=0, board=board, moves=moves)
 
     def actions(self, state):
         """Return a list of the legal moves at this point."""
-        raise NotImplementedError
+        return [pos for pos in state.board if state.board[pos] is None]
 
     def result(self, state, move):
         """Return the state that results from making a move from a state."""
-        raise NotImplementedError
+        # Make sure the move is valid
+        if move not in state.board or state.board[move] is not None:
+            return "INVALID"
+        
+        # Create a new board with the new move
+        new_board = state.board.copy()
+        new_board[move] = state.to_move
+
+        # Remove the move from the list of available moves
+        new_moves = [m for m in state.moves if m != move]
+
+        # Switch to the other player
+        next_player = 'O' if state.to_move == 'X' else 'X'
+
+        return GameState(to_move=next_player, utility=0, board=new_board, moves=new_moves)
 
     def utility(self, state, player):
         """Return the value of this final state to player."""
-        raise NotImplementedError
+        # TODO: add in logic to check for mills or other winning/losing conditions
+        
+        # Check if the player has won
+        if self.terminal_test(state):
+            if self.check_win(state, player):
+                return 1
+            elif self.check_win(state, 'O' if player == 'X' else 'X'):
+                return -1
+        return 0
 
     def terminal_test(self, state):
         """Return True if this is a final state for the game."""
@@ -79,19 +106,6 @@ class Lasker_Morris(Game):
 
     def __repr__(self):
         return '<{}>'.format(self.__class__.__name__)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def alpha_beta_search(state, game):
@@ -159,14 +173,14 @@ def main():
     # Read initial color/symbol
     player_id = input().strip()
     first_move_made = 0
-    tic = TicTacToe(3, 3, 3)
+    tic = Lasker_Morris(3, 3, 3)
     tac = tic.initial #gamestate
     while True:
         # first move logic
         if player_id == "blue" and first_move_made == 0:
             moveX1 = (alpha_beta_search(tac, tic)) #get best 1st move as X
             # update internal board with our move
-            tac = TicTacToe.result(tic, tac, moveX1)
+            tac = Lasker_Morris.result(tic, tac, moveX1)
             first_move_made = first_move_made + 1
             print(tuple_translate(moveX1), flush=True) #send move to referee
 
@@ -179,15 +193,15 @@ def main():
                     print("blue player has played an invalid move; orange player wins!", flush=True)
                     sys.exit(0)
                 # update internal board with opponent move
-                tac = TicTacToe.result(tic, tac, translated_op_inputX)
+                tac = Lasker_Morris.result(tic, tac, translated_op_inputX)
                 if tac == "INVALID":
                     print("blue player has played an invalid move; orange player wins!", flush=True)
                     sys.exit(0)
                 moveO1 = alpha_beta_search(tac, tic) #get best move as O
-                tac = TicTacToe.result(tic, tac, moveO1)
+                tac = Lasker_Morris.result(tic, tac, moveO1)
                 print(tuple_translate(moveO1), flush=True)
                 #check for orange win and terminate if win is found
-                if TicTacToe.terminal_test(tic, tac) and tac.utility == 1:
+                if Lasker_Morris.terminal_test(tic, tac) and tac.utility == 1:
                     print("GAME OVER: orange player wins!")
                     sys.exit(0)
 
@@ -199,19 +213,22 @@ def main():
                 print("orange player has played an invalid move; blue player wins!", flush=True)
                 sys.exit(0)
             # update internal board with opponent move
-            tac = TicTacToe.result(tic, tac, translated_op_inputO)
+            tac = Lasker_Morris.result(tic, tac, translated_op_inputO)
             if tac == "INVALID":
                 print("orange player has played an invalid move; blue player wins!", flush=True)
             # Your move logic here
             moveX2 = alpha_beta_search(tac, tic)  # get best move as X
-            tac = TicTacToe.result(tic, tac, moveX2)
+            tac = Lasker_Morris.result(tic, tac, moveX2)
             # Send move to referee
             print(tuple_translate(moveX2), flush=True)
             # check for blue win and terminate if win is found
-            if TicTacToe.terminal_test(tic, tac) and tac.utility == 1:
+            if Lasker_Morris.terminal_test(tic, tac) and tac.utility == 1:
                 print("GAME OVER: blue player wins!")
                 sys.exit(0)
 
-            if TicTacToe.terminal_test(tic, tac) and tac.utility == 0:
+            if Lasker_Morris.terminal_test(tic, tac) and tac.utility == 0:
                 print("GAME OVER: it's a draw!")
                 sys.exit(0)
+        except Exception as e:
+            print("Error:", e)
+            sys.exit(1)
